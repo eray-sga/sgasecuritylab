2908362774dd3e4e72deb016c6099619fca14037
Eray Kalkan <ery.kalkan19@outlook.com>
2021-12-09 10:13:02 +0300

    Update db.php

--- system/db.php
+++ system/db.php
@@ -1,6 +1,7 @@
 <?php
 try {
      $db = new PDO("mysql:host=localhost;dbname=securitylab", "root", "");
+     $db->exec("SET NAMES 'utf8'; SET CHARSET 'utf8'");
 } catch ( PDOException $e ){
      echo $e->getMessage();
 }
